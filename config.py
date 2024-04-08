6a931af14c68a369beeb5ee5a456967c427ca87b
alber <albert.mazuz@gmail.com>
2024-04-07 23:41:49 -0300

    ajuste config

---
+++ config.py
@@ -0,0 +1,7 @@
+import streamlit as st
+databaseSuperUser=st.secrets['databaseSuperUser']
+
+database=st.secrets['database']
+
+connection_string=st.secrets['connection_string']
+# connection_string=r'postgres://postgres:30794009@localhost:5432/recommendationSystems'
